158fb00103959246a46a1fc556bf92ba840b7e32
Robert Sahakyan <robsahakyan54@gmail.com>
2025-10-08 23:18:40 +0400

    Added case of finish interview auto close

--- src/constants/completion-reason.enum.ts
+++ src/constants/completion-reason.enum.ts
@@ -1,5 +1,6 @@
 export enum CompletionReasonEnum {
   NORMAL = 'NORMAL',
+  TIME_PASSED = 'TIME_PASSED',
   TAB_CLOSE = 'TAB_CLOSE',
   USER_EXIT = 'USER_EXIT',
   SYSTEM_ERROR = 'SYSTEM_ERROR'
@@ -12,6 +13,7 @@ export enum CompletionTypeEnum {
 
 export const completionReasonVisibleContent = {
   [CompletionReasonEnum.NORMAL]: 'Normal Completion',
+  [CompletionReasonEnum.TIME_PASSED]: 'Time Passed',
   [CompletionReasonEnum.TAB_CLOSE]: 'Tab Close',
   [CompletionReasonEnum.USER_EXIT]: 'User Exit',
   [CompletionReasonEnum.SYSTEM_ERROR]: 'System Error'
